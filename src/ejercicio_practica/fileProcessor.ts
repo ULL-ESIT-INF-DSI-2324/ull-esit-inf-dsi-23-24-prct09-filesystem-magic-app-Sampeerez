527492738dd20b85a60768b42fde161c020c143d
Sampeerez <alu0101481524@ull.edu.es>
2024-03-20 11:18:33 +0000

    Formateado con prettier

--- src/ejercicio_practica/fileProcessor.ts
+++ src/ejercicio_practica/fileProcessor.ts
@@ -1,19 +1,21 @@
-
-import { Item } from './item.js';
-import fs from 'fs';
+import { Item } from "./item.js";
+import fs from "fs";
 
 export abstract class FileProcessor {
-  process(filename: string): {benefits: number[], weights: number[]} {
+  process(filename: string): { benefits: number[]; weights: number[] } {
     const content = this.readFile(filename);
     const data = this.parseContent(content);
     return this.extractData(data);
   }
 
   protected readFile(filename: string): string {
-    return fs.readFileSync(filename, 'utf8');
+    return fs.readFileSync(filename, "utf8");
   }
 
   public abstract parseContent(content: string): Item[];
 
-  public abstract extractData(data: Item[]): {benefits: number[], weights: number[]};
+  public abstract extractData(data: Item[]): {
+    benefits: number[];
+    weights: number[];
+  };
 }
--- tests/ejercicio-1.spec.ts
+++ tests/ejercicio-1.spec.ts
@@ -1,30 +1,36 @@
-import 'mocha';
-import { expect } from 'chai';
-import { JSONProcessor } from '../src/ejercicio_practica/jsonProcessor.js';
-import { CSVProcessor } from '../src/ejercicio_practica/csvProcessor.js';
-import * as path from 'path';
-import { fileURLToPath } from 'url';
+import "mocha";
+import { expect } from "chai";
+import { JSONProcessor } from "../src/ejercicio_practica/jsonProcessor.js";
+import { CSVProcessor } from "../src/ejercicio_practica/csvProcessor.js";
+import * as path from "path";
+import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-describe('FileProcessor', () => {
-  it('should correctly process JSON content', () => {
+describe("FileProcessor", () => {
+  it("should correctly process JSON content", () => {
     const processor = new JSONProcessor();
-    const filename = path.resolve(__dirname, '../src/ejercicio_practica/ejemplo_json.json'); 
+    const filename = path.resolve(
+      __dirname,
+      "../src/ejercicio_practica/ejemplo_json.json",
+    );
     const result = processor.process(filename);
     expect(result).to.deep.equal({
       benefits: [60, 100, 120],
-      weights: [10, 20, 30]
+      weights: [10, 20, 30],
     });
   });
 
-  it('should correctly process CSV content', () => {
+  it("should correctly process CSV content", () => {
     const processor = new CSVProcessor();
-    const filename = path.resolve(__dirname, '../src/ejercicio_practica/ejemplo_csv.csv'); 
+    const filename = path.resolve(
+      __dirname,
+      "../src/ejercicio_practica/ejemplo_csv.csv",
+    );
     const result = processor.process(filename);
     expect(result).to.deep.equal({
       benefits: [60, 100, 120],
-      weights: [10, 20, 30]
+      weights: [10, 20, 30],
     });
   });
 });
