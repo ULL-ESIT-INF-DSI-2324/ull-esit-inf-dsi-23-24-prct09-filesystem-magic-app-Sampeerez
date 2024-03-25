622e5bd688ba41c9f91db179f9c6e922cddd471a
Sampeerez <alu0101481524@ull.edu.es>
2024-03-25 00:04:14 +0000

    Añadidas pruebas

--- tests/ejercicio-1.spec.ts
+++ tests/ejercicio-1.spec.ts
@@ -4,6 +4,7 @@ import { JSONProcessor } from "../src/ejercicio_practica/jsonProcessor.js";
 import { CSVProcessor } from "../src/ejercicio_practica/csvProcessor.js";
 import * as path from "path";
 import { fileURLToPath } from "url";
+import { Card, Color, Type, Rarity } from '../src/ejercicio-1/card.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -64,5 +65,63 @@ describe("FileProcessor", () => {
       benefits: [60, 100, 120],
       weights: [10, 20, 30],
     });
+  });
+});
+
+describe('Magic Cards', () => {
+  it('should create a new Card instance', () => {
+    const card = new Card(1, 'Nombre de la carta', 3, Color.Blue, Type.Land, Rarity.Common, 'Texto de las reglas de la carta', 10);
+    expect(card).to.be.an.instanceOf(Card);
+    expect(card.getId()).to.equal(1);
+    expect(card.getNombre()).to.equal('Nombre de la carta');
+    expect(card.getCosteDeMana()).to.equal(3);
+    expect(card.getColor()).to.equal(Color.Blue);
+    expect(card.getTipo()).to.equal(Type.Land);
+    expect(card.getRareza()).to.equal(Rarity.Common);
+    expect(card.getTextoDeReglas()).to.equal('Texto de las reglas de la carta');
+    expect(card.getValorDeMercado()).to.equal(10);
+    expect(card.getFuerzaYResistencia()).to.be.null;
+    expect(card.getMarcasDeLealtad()).to.be.null;
+  });
+
+  it('should create a new Card instance with FuerzaYResistencia', () => {
+    const card = new Card(1, 'Nombre de la carta', 3, Color.Blue, Type.Creature, Rarity.Common, 'Texto de las reglas de la carta', 10, [2, 2]);
+    expect(card).to.be.an.instanceOf(Card);
+    expect(card.getId()).to.equal(1);
+    expect(card.getNombre()).to.equal('Nombre de la carta');
+    expect(card.getCosteDeMana()).to.equal(3);
+    expect(card.getColor()).to.equal(Color.Blue);
+    expect(card.getTipo()).to.equal(Type.Creature);
+    expect(card.getRareza()).to.equal(Rarity.Common);
+    expect(card.getTextoDeReglas()).to.equal('Texto de las reglas de la carta');
+    expect(card.getValorDeMercado()).to.equal(10);
+    expect(card.getFuerzaYResistencia()).to.deep.equal([2, 2]);
+    expect(card.getMarcasDeLealtad()).to.be.null;
+  });
+
+  it('should create a new Card instance with MarcasDeLealtad', () => {
+    const card = new Card(
+      1,
+      'Nombre de la carta',
+      3,
+      Color.Blue,
+      Type.Planeswalker,
+      Rarity.Common,
+      'Texto de las reglas de la carta',
+      10,
+      null,
+      3,
+    );
+    expect(card).to.be.an.instanceOf(Card);
+    expect(card.getId()).to.equal(1);
+    expect(card.getNombre()).to.equal('Nombre de la carta');
+    expect(card.getCosteDeMana()).to.equal(3);
+    expect(card.getColor()).to.equal(Color.Blue);
+    expect(card.getTipo()).to.equal(Type.Planeswalker);
+    expect(card.getRareza()).to.equal(Rarity.Common);
+    expect(card.getTextoDeReglas()).to.equal('Texto de las reglas de la carta');
+    expect(card.getValorDeMercado()).to.equal(10);
+    expect(card.getFuerzaYResistencia()).to.be.null;
+    expect(card.getMarcasDeLealtad()).to.equal(3);
   });
 });
